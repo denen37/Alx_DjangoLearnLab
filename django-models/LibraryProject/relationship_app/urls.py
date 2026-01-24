8f042ca7271ab2bf2bb2f56074804c7efd49a963
Denen <awardenen@gmail.com>
2026-01-24 00:01:52 +0000

    Correction 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import CustomLoginView, CustomRegisterView, index
+from .views import login_view, logout_view, index, register_view
 from .views import list_books
 from .views import LibraryDetailView
 
 urlpatterns = [
    path('', index, name='index'),
-   path('login/', CustomLoginView.as_view(), name='login'),
-   path('register/', CustomRegisterView.as_view(), name='register'),
+   path('login/', login_view, name='login'),
+   path('logout/', logout_view, name='logout'),
+   path('register/', register_view, name='register'),
    path('books/', list_books, name='book_list'),
    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
