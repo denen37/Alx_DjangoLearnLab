f796097189a1391940cf715828a510cf665508ea
Denen <awardenen@gmail.com>
2026-01-24 00:08:22 +0000

    Correction 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
 from .models import Library
 from django.contrib.auth import authenticate, login, logout
@@ -8,35 +12,35 @@ from django.contrib.auth import authenticate, login, logout
 def index(request):
     return HttpResponse("<h1 style='text-align: center;'>Welcome to the Library</h1>")
 
-# class CustomLoginView(LoginView):
-#     template_name = 'relationship_app/login.html'
-#     next_page = 'books/'
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    next_page = 'books/'
 
-# class CustomLogoutView(LogoutView):
-#     template_name = 'relationship_app/logout.html'
-#     next_page = 'login'
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    next_page = 'login'
 
 
-# class CustomRegisterView(CreateView):
-#     template_name = 'relationship_app/register.html'
-#     form_class = UserCreationForm
-#     success_url = reverse_lazy('login')
+class CustomRegisterView(CreateView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
 def register_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = User.objects.create_user(username=username, password=password)
-        login(request, user)
-        return redirect('books/')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('books/')
     else:
         return render(request, 'relationship_app/register.html')
     
 def login_view(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             # Redirect to a success page.
             return redirect('books/')
